b8d3d8be43864659ad2509c71ddbba38e0abdad2
rafaeloledo <rafaeloliveiraledo@gmail.com>
2024-04-14 15:16:34 -0300

    chore(cpp): register the class

--- program.cpp
+++ program.cpp
@@ -8,6 +8,19 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hINSTANCE, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
+	// Register the window class
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+	WNDCLASS wc = {};
+
+	wc.lpfWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	// Create the window
+
 	return 0;
 }
 
